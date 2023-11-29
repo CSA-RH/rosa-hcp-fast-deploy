9ab0c16b9090875225db094d8376eb8644e135ec
GianfrancoMollo <gmollo@redhat.com>
2023-11-29 13:22:35 +0100

    local update

--- create_bastion_host.sh
+++
@@ -1,8 +0,0 @@
-aws ec2 run-instances \
-    --image-id ami-06d4b7182ac3480fa \
-    --count 1 \
-    --instance-type t2.micro \
-    --key-name bastion-host \
-    --security-group-ids sg-07570e17ab8331f13 \
-    --subnet-id $Public_Subnet \
-    --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=bastion-host}]'
