fa8dcf5d815f041a0168aa95b8e832177e8a2533
GianfrancoMollo <gmollo@redhat.com>
2023-12-11 12:17:23 +0100

    local update

--- rosa_hcp.sh
+++ rosa_hcp.sh
@@ -141,7 +141,7 @@ sleep 5
 echo " " 2>&1 |tee -a $CLUSTER_LOG
 echo " " 2>&1 |tee -a $CLUSTER_LOG
 echo " " 2>&1 |tee -a $CLUSTER_LOG
-exit 0
+Fine
 }
 #
 ############################################################
@@ -249,7 +249,7 @@ sleep 5
 echo " " 2>&1 |tee -a $CLUSTER_LOG
 echo " " 2>&1 |tee -a $CLUSTER_LOG
 echo " " 2>&1 |tee -a $CLUSTER_LOG
-exit 0
+Fine
 }
 #
 ############################################################
@@ -387,7 +387,7 @@ sleep 5
 echo " " 2>&1 |tee -a $CLUSTER_LOG
 echo " " 2>&1 |tee -a $CLUSTER_LOG
 echo " " 2>&1 |tee -a $CLUSTER_LOG
-exit 0
+Fine
 }
 #
 ############################################################
@@ -425,7 +425,7 @@ echo "Start deleting VPC ${VPC_ID} " 2>&1 |tee -a $CLUSTER_LOG
    while read -r instance_id ; do aws ec2 delete-nat-gateway --nat-gateway-id $instance_id; done < <(aws ec2 describe-nat-gateways | jq -r '.NatGateways[].NatGatewayId') 2>&1 >> $CLUSTER_LOG
 # NOTE: waiting for the NAT-GW to die - se non crepa non andiamo da nessuna parte
 echo "waiting for the NAT-GW to die " 2>&1 |tee -a $CLUSTER_LOG
-#sleep 100
+sleep 100
 #
     while read -r sg ; do aws ec2 delete-security-group --no-cli-pager --group-id $sg 2>&1 >> $CLUSTER_LOG; done < <(aws ec2 describe-security-groups --filters 'Name=vpc-id,Values='$VPC_ID | jq -r '.SecurityGroups[].GroupId') 2>&1 >> $CLUSTER_LOG
     while read -r acl ; do  aws ec2 delete-network-acl --network-acl-id $acl 2>&1 >> $CLUSTER_LOG; done < <(aws ec2 describe-network-acls --filters 'Name=vpc-id,Values='$VPC_ID| jq -r '.NetworkAcls[].NetworkAclId') 2>&1 >> $CLUSTER_LOG
@@ -438,7 +438,7 @@ echo "waiting for the NAT-GW to die " 2>&1 |tee -a $CLUSTER_LOG
 aws ec2 delete-vpc --vpc-id=$VPC_ID 2>&1 >> $CLUSTER_LOG
 echo "VPC ${VPC_ID} deleted !" 2>&1 |tee -a $CLUSTER_LOG
 #
-rosa delete account-roles --mode auto --prefix $PREFIX --yes 2>&1 |tee -a $CLUSTER_LOG
+rosa delete account-roles --mode auto --prefix $PREFIX --yes 2>&1 >> $CLUSTER_LOG
 echo "account-roles deleted !" 2>&1 |tee -a $CLUSTER_LOG
 #
 echo "#" 2>&1 |tee -a $CLUSTER_LOG
@@ -450,7 +450,7 @@ echo "The old LOG file ${CLUSTER_LOG} in is now moved to /tmp folder" 2>&1 |tee 
 echo " " 2>&1 |tee -a $CLUSTER_LOG
 mv $CLUSTER_LOG /tmp
 Countdown
-exit 0
+Fine
 }
 #
 mainmenu() {
